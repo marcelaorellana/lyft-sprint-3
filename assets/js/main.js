1ead844bce16432e1f6d792cf50d398440a534a8
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-05-24 10:59:27 -0400

    no se mueve la pantalla al jugar

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,22 @@ var botonStart = document.getElementById("startId");
 botonStart.addEventListener("click", function(){
 	var coorX = parseInt(document.getElementById("inputAuto1").value);
 	var coorY = parseInt(document.getElementById("inputAuto2").value);
-	document.getElementById("playWithId").classList.toggle("no");
-	console.log(coorX + coorY);
-	document.getElementById("juegoId").classList.remove("no");
-
 	var autoMu = document.getElementById("autito");
-
 	var coorX50 = (coorX * 80);
 	var coorY50 = (coorY * 80);
+
+	document.getElementById("playWithId").classList.toggle("no");
+	
+	document.getElementById("juegoId").classList.remove("no");
+
+	console.log(coorX + coorY);
+
 	autoMu.style.left = coorX50 + "px";
 	autoMu.style.top = coorY50 + "px";
 
-
-
 function animacion(e){
 	if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40){
-
+		e.preventDefault();
 		var pintado = document.createElement("div");
 		pintado.classList.add("rosa");
 		pintado.style.left = coorX50 + "px";
@@ -28,7 +28,7 @@ function animacion(e){
 		console.log(coorX50 +"X" +coorY50);
 
 		if(e.keyCode == 39){//right
-			if(coorX50 < 750){
+			if(coorX50 < 720){
 				coorX50 += 80;
 				autoMu.style.left = coorX50 + "px";
 			}
@@ -57,6 +57,7 @@ function animacion(e){
 document.onkeydown = animacion;
 });
 
+//validación del numero de telefono el en header
 function validacionNumero(){
 	var botonNumero = document.getElementById("becomeId");
 
